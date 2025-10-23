89ec340884d22e2c9d6ede7a1dfc22f4deab0e87
Basant Refaat <b.refaat@coreations.com>
2025-10-23 11:07:07 +0300

    add GetBranchByIdFreature , Add CheckBranchIsExist Query

---
+++ HrManagementSystem/Features/BranchManagement/GetBranchById/Queries/GetBranchByIdQuery.cs
@@ -0,0 +1,32 @@
+﻿using HrManagementSystem.Common;
+using HrManagementSystem.Common.Entities;
+using HrManagementSystem.Common.Enums;
+using HrManagementSystem.Common.Views;
+using HrManagementSystem.Features.BranchManagement.GetBranchById.Queries.Dtos;
+using Mapster;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace HrManagementSystem.Features.BranchManagement.GetBranchById.Queries
+{
+    public record GetBranchByIdQuery(string BranchId) : IRequest<RequestResult<BranchDto>>;
+    public class GetBranchByIdQueryHandler : RequestHandlerBase<GetBranchByIdQuery, RequestResult<BranchDto>, Branch>
+    {
+        public GetBranchByIdQueryHandler(RequestHandlerBaseParameters<Branch> parameters) : base(parameters)
+        {
+        }
+
+        public async override Task<RequestResult<BranchDto>> Handle(GetBranchByIdQuery request, CancellationToken cancellationToken)
+        {
+            var branch = await _repository.Get(b => b.Id == request.BranchId)
+                .ProjectToType<BranchDto>().FirstOrDefaultAsync(cancellationToken);
+
+
+            if (branch == null)
+                return RequestResult<BranchDto>.Failure("Branch not found", ErrorCode.BranchNotExist);
+
+            return RequestResult<BranchDto>.Success(branch);
+        }
+    }
+
+}
---
+++ HrManagementSystem/Features/BranchManagement/GetBranchById/Validators/GetBranchByIdRequestViewModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using HrManagementSystem.Features.LocationManagement.CountryManagement.DeleteCountry;
+
+namespace HrManagementSystem.Features.BranchManagement.GetBranchById.Validators
+{
+    public class GetBranchByIdRequestViewModelValidator : AbstractValidator<GetBranchByIdRequestViewModel>
+    {
+        public GetBranchByIdRequestViewModelValidator()
+        {
+            RuleFor(x => x.BranchId)
+                .NotEmpty().WithMessage("BranchId is required");
+        }
+    }
+}
