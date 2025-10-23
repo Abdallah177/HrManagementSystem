a7f32174a28d1fef3c56df3f785782eea6e6ebf2
Basant Refaat <b.refaat@coreations.com>
2025-10-23 13:57:27 +0300

    Adddepartment Freature

---
+++ HrManagementSystem/Features/DepartmentManagement/AddDepartment/Validators/AddDepartmentRequestViewModelValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace HrManagementSystem.Features.DepartmentManagement.AddDepartment.Validators
+{
+    public class AddDepartmentRequestViewModelValidator : AbstractValidator<AddDepartmentRequestViewModel>
+    {
+        public AddDepartmentRequestViewModelValidator()
+        {
+            RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Department name is required")
+            .MaximumLength(100).WithMessage("Department name cannot exceed 100 characters");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Description cannot exceed 500 characters");
+
+            RuleFor(x => x.BranchId)
+                .NotEmpty().WithMessage("BranchId is required");
+        }
+    }
+}
