1061a92060b0533c7f6cf6b653c54a16a4f57399
ahmed01114790265 <01114790265ahmed.com>
2025-10-24 08:02:12 +0300

    Create api that getDepartmentById .

---
+++ HrManagementSystem/Features/DepartmentManagement/GetDepartmentById/Validator/GetDepartmentByIdRequestViewModelValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace HrManagementSystem.Features.DepartmentManagement.GetDepartmentById.Validator
+{
+    public class GetDepartmentByIdRequestViewModelValidator : AbstractValidator<GetDepartmentByIdRequestViewModel>
+    {
+        public GetDepartmentByIdRequestViewModelValidator()
+        {
+            RuleFor(x => x.DepartmentId)
+                .NotEmpty().WithMessage("DepartmentId is required");
+        }
+    }
+}
