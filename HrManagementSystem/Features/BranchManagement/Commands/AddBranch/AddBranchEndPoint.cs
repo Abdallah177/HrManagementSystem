d1e67714250a913ff7f073e714c7d9ca428c1ebb
ahmed01114790265 <01114790265ahmed.com>
2025-10-24 06:34:53 +0300

    create api that add branch

---
+++ HrManagementSystem/Features/BranchManagement/Commands/AddBranch/AddBranchEndPoint.cs
@@ -0,0 +1,32 @@
+﻿using HrManagementSystem.Common;
+using HrManagementSystem.Common.Views;
+using HrManagementSystem.Features.BranchManagement.Commands.AddBranch.Commands;
+using Mapster;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
+
+namespace HrManagementSystem.Features.BranchManagement.Commands.AddBranch
+{
+    public class AddBranchEndPoint : BaseEndPoint<AddBranchRquestViewModel, AddBranchResponseViewModel>
+    {
+        public AddBranchEndPoint(EndpointBaseParameters<AddBranchRquestViewModel> parameters) : base(parameters)
+        {
+        }
+        [HttpPost]
+        public async Task<EndpointResponse<AddBranchResponseViewModel>> AddBranch(AddBranchRquestViewModel viewModel, CancellationToken cancellationToken)
+        {
+            var validationResult = ValidateRequest(viewModel);
+            if (!validationResult.IsSuccess)
+            {
+                return validationResult;
+            }
+            var result = await _mediator.Send(new AddBranchCommand(viewModel.Name,viewModel.CityId,viewModel.Phone,viewModel.CompanyId,viewModel.UserId));
+            if (!result.IsSuccess)
+                return EndpointResponse<AddBranchResponseViewModel>.Failure(result.Message, result.ErrorCode);
+            var addBranchResponseViewModel = result.Data.Adapt<AddBranchResponseViewModel>();
+            return EndpointResponse<AddBranchResponseViewModel>.Success(addBranchResponseViewModel);
+        }
+
+    }
+    
+}
