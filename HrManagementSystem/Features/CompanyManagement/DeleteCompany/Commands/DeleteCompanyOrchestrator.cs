8f587f34a28e5e4f6ad541fc2db9f35894f312d2
Basant Refaat <b.refaat@coreations.com>
2025-10-27 15:50:56 +0300

    add DeleteCompanyOrchestrator

---
+++ HrManagementSystem/Features/CompanyManagement/DeleteCompany/Commands/DeleteCompanyOrchestrator.cs
@@ -0,0 +1,22 @@
+﻿using HrManagementSystem.Common;
+using HrManagementSystem.Common.Entities;
+using HrManagementSystem.Common.Enums;
+using HrManagementSystem.Common.Views;
+using MediatR;
+
+namespace HrManagementSystem.Features.CompanyManagement.DeleteCompany.Commands
+{
+    public record DeleteCompanyOrchestrator(string companyId) : IRequest<RequestResult<bool>>;
+
+    public class DeleteCompanyOrchestratorHandler : RequestHandlerBase<DeleteCompanyOrchestrator, RequestResult<bool>, Company>
+    {
+        public DeleteCompanyOrchestratorHandler(RequestHandlerBaseParameters<Company> parameters) : base(parameters)
+        {
+        }
+
+        public override Task<RequestResult<bool>> Handle(DeleteCompanyOrchestrator request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
