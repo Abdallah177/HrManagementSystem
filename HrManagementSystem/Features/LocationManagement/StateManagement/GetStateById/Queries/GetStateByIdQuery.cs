ed5c6289da7532cdfb747826b0b48d9e25ddc745
nada abdelmonem <nadaabdelmonem953@gmail.com>
2025-10-27 22:19:44 +0300

    ....................

--- HrManagementSystem/Features/LocationManagement/StateManagement/GetStateById/Queries/GetStateByIdQuery.cs
+++ HrManagementSystem/Features/LocationManagement/StateManagement/GetStateById/Queries/GetStateByIdQuery.cs
@@ -2,24 +2,37 @@
 using HrManagementSystem.Common.Entities.Location;
 using HrManagementSystem.Common.Enums;
 using HrManagementSystem.Common.Views;
+using HrManagementSystem.Features.LocationManagement.StateManagement.GetStateById.Queries.Dto;
+using HrManagementSystem.Features.OrganizationManagement.GetAllOrganization.Queries.Dtos;
+using Mapster;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace HrManagementSystem.Features.LocationManagement.StateMangement.GetStateById.Queries
 {
-    public record GetStateByIdQuery(string Id) : IRequest<RequestResult<State?>>;
+    public record GetStateByIdQuery(string Id) : IRequest<RequestResult<GetByIdStateDto?>>;
 
 
-    public class GetStateByIdQueryHandler : RequestHandlerBase<GetStateByIdQuery, RequestResult<State?>, State>
+    public class GetStateByIdQueryHandler : RequestHandlerBase<GetStateByIdQuery, RequestResult<GetByIdStateDto?>, State>
     {
         public GetStateByIdQueryHandler(RequestHandlerBaseParameters<State> requestHandlerBaseParameters) : base(requestHandlerBaseParameters) { }
 
-        public override async Task<RequestResult<State?>> Handle(GetStateByIdQuery request, CancellationToken cancellationToken)
+        public override async Task<RequestResult<GetByIdStateDto?>> Handle(GetStateByIdQuery request, CancellationToken cancellationToken)
         {
-            var state = await _repository.GetByIDAsync(request.Id, cancellationToken);
-            if (state == null)
-                return RequestResult<State?>.Failure("State is not found", ErrorCode.StateNotFound);
 
-            return RequestResult<State?>.Success(state);
+
+            var state = await _repository
+                    .GetAll() 
+                    .Where(s => s.Id == request.Id)
+                    .ProjectToType<GetByIdStateDto>() 
+                    .FirstOrDefaultAsync(cancellationToken);
+
+           
+            if (state == null)
+                return RequestResult<GetByIdStateDto?>.Failure("State is not found", ErrorCode.StateNotFound);
+
+         
+            return RequestResult<GetByIdStateDto?>.Success(state);
         }
     }
 }
