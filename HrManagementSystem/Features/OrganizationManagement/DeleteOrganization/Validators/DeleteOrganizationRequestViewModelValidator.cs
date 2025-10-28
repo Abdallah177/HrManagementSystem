1a53bd50258b34e92b2e84196fc34cfbacd9bf13
ahmed01114790265 <01114790265ahmed.com>
2025-10-28 17:39:53 +0300

    create api that delete organization

---
+++ HrManagementSystem/Features/OrganizationManagement/DeleteOrganization/Validators/DeleteOrganizationRequestViewModelValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace HrManagementSystem.Features.OrganizationManagement.DeleteOrganization.Validators
+{
+    public class DeleteOrganizationRequestViewModelValidator : AbstractValidator<DeleteOrganizationRequestViewModel>
+    {
+        public DeleteOrganizationRequestViewModelValidator()
+        {
+            RuleFor(x => x.OrganizationId)
+                .NotEmpty().WithMessage("OrganizationId is required.");
+        }
+    }
+}
