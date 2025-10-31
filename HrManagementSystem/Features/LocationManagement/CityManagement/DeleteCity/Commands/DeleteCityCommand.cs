4710df3bf87157f66d7f59cf3b1b9e08dbdb6df1
Abdallah177 <abdallahelshawry6@gmail.com>
2025-10-31 22:46:57 +0200

    RefactorDeleteCityCommand

--- HrManagementSystem/Features/LocationManagement/CityManagement/DeleteCity/Commands/DeleteCityCommand.cs
+++ HrManagementSystem/Features/LocationManagement/CityManagement/DeleteCity/Commands/DeleteCityCommand.cs
@@ -2,29 +2,24 @@
 using HrManagementSystem.Common.Entities.Location;
 using HrManagementSystem.Common.Enums;
 using HrManagementSystem.Common.Views;
+using HrManagementSystem.Features.LocationManagement.CityManagement.Queries.GetByIDCity.Queries;
 using MediatR;
 
 namespace HrManagementSystem.Features.LocationManagement.CityManagement.DeleteCity.Commands
 {
     public record DeleteCityCommand(string Id, string UserId) : IRequest<RequestResult<bool>>;
 
-    public class DeleteCityCommandHandler : RequestHandlerBase<DeleteCityCommand, RequestResult<bool>, State>
+    public class DeleteCityCommandHandler : RequestHandlerBase<DeleteCityCommand, RequestResult<bool>, City>
     {
-        public DeleteCityCommandHandler(RequestHandlerBaseParameters<State> parameters) : base(parameters) { }
+        public DeleteCityCommandHandler(RequestHandlerBaseParameters<City> parameters) : base(parameters) { }
         public override async Task<RequestResult<bool>> Handle(DeleteCityCommand request, CancellationToken cancellationToken)
         {
-            // Will Be Refactoring After Making Get By Id
-            var city = await _repository.GetByIDAsync(request.Id, cancellationToken);
-            if (city == null)
+            var city = await _mediator.Send(new GetCityByIdQuery(request.Id));
+            if (!city.IsSuccess)
                 return RequestResult<bool>.Failure("City Not Found", ErrorCode.NoCitiesfound);
 
 
-            city.IsDeleted = true;
-            city.IsActive = false;
-            city.UpdatedByUser = request.UserId;
-            city.UpdatedAt = DateTime.UtcNow;
-
-            _repository.Update(city, request.UserId);
+            await _repository.DeleteAsync(request.Id, request.UserId,cancellationToken);
 
             return RequestResult<bool>.Success(true, "City deleted successfully");
         }
