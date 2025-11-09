298c3ed54737afb64cde9841815f263ab787a798
mahmoud abdelaziz <mahmoudabdelazi962@gmail.com>
2025-11-09 19:23:28 +0200

    GetVacationById&FixErorrCodeEnums

--- HrManagementSystem/Common/Enums/ErrorCode.cs
+++ HrManagementSystem/Common/Enums/ErrorCode.cs
@@ -2,44 +2,56 @@
 {
     public enum ErrorCode
     {
-        NoError = 1 ,
-        DuplicateRecord= 2 ,
-
-        CountryNotFound =100,
-        CountryHasStates = 101,
-        CountryHasRelatedCompanies = 102,
-        CountryNameIsExist = 103,
-
-        StateNotFound = 200,
-        StateIsExist = 201,
-
-        NoCitiesfound = 300,
-        CityAlreadyExistsInThisState = 301 ,
-        CityNotExist = 302,
-        DuplicateCityName = 303,
-
-        OrganizationNotExis = 400,
-        NoOrganizationFound = 401,
-        OrganizationIDNotFound =402,
-        OrganizationAlreadyExists=403,
-
-        CompanyNotExist = 500,
-
-        BranchNotExist = 600,
-        CanNotRemoveThisBranch = 601,
-
-        DepartmentNotExist = 700,
-        DepartmentIsExist = 701,
-        DepartmentHasRelatedTeams = 702,
-
-        StateHasCities =201,
-
-        TeamNotExist = 800,
-        NoTeamsFound = 801,
-
-        ProbationNotFound = 900,
-        ProbationNotExist = 901,    
+        //------------ General (0– 99)---------------
+        NoError = 1,
+        DuplicateRecord = 2,
 
 
+        //--------- Main Entity (100–199)--------------
+        OrganizationNotExis = 100,
+        OrganizationIDNotFound = 101,
+        NoOrganizationFound = 102,
+        OrganizationAlreadyExists = 103,
+
+        
+        CompanyNotExist = 110,
+
+       
+        BranchNotExist = 120,
+        CanNotRemoveThisBranch = 121,
+
+       
+        DepartmentNotExist = 130,
+        DepartmentIsExist = 131,
+        DepartmentHasRelatedTeams = 132,
+
+        
+        TeamNotExist = 140,
+        NoTeamsFound = 141,
+
+        //--------------------Location (200–299)-----------
+        CountryNotFound = 200, 
+        CountryHasStates = 201,
+        CountryHasRelatedCompanies = 202,
+        CountryNameIsExist = 203,
+
+       
+        StateNotFound = 210, 
+        StateIsExist = 211,
+        StateHasCities = 212,
+
+       
+        NoCitiesfound = 220,
+        CityAlreadyExistsInThisState = 221,
+        CityNotExist = 222,
+        DuplicateCityName = 223,
+
+
+        //--------------Configurations(300–399)----------
+        ProbationNotFound = 300,
+        ProbationNotExist = 301,
+
+        
+        VacationNotFound = 310,
     }
 }
