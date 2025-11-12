37b7dcf01ea06985eaddf250f362457a0ea547b7
Abdallah177 <abdallahelshawry6@gmail.com>
2025-11-12 14:09:23 +0200

    EditErrorCode

--- HrManagementSystem/Common/Enums/ErrorCode.cs
+++ HrManagementSystem/Common/Enums/ErrorCode.cs
@@ -54,8 +54,6 @@
         
         VacationNotFound = 310,
         VacationNotExist = 311,
-        ProbationNotFound = 900,
-        ProbationNotExist = 901,
         RequestNotExist = 902,
         RequestNotFound = 903,
     }
