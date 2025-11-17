f54908385fb99df710dae269541444faf7f83d4a
mahmoud abdelaziz <mahmoudabdelazi962@gmail.com>
2025-11-17 16:17:27 +0200

    AddVacationCommand

---
+++ HrManagementSystem/Features/ConfigurationsManagement/VacationMangement/AddVacation/Command/AddVacationCommand.cs
@@ -0,0 +1,37 @@
+﻿using HrManagementSystem.Common;
+using HrManagementSystem.Common.Entities.Features;
+using HrManagementSystem.Common.Enums;
+using HrManagementSystem.Common.Enums.FeatureEnums;
+using HrManagementSystem.Common.Views;
+using HrManagementSystem.Features.ConfigurationsManagement.VacationMangement.AddVacation.DTOs;
+using HrManagementSystem.Features.ConfigurationsManagement.VacationMangement.AddVacation.Queries;
+using Mapster;
+using MediatR;
+
+namespace HrManagementSystem.Features.ConfigurationsManagement.VacationMangement.AddVacation.Command
+{
+    public record AddVacationCommand(string Name, VacationType Type, int DurationInDays, string? Description) : IRequest<RequestResult<AddVacationDto>>;
+
+    public class AddVacationCommandHandler : RequestHandlerBase<AddVacationCommand, RequestResult<AddVacationDto>, Vacation>
+    {
+        public AddVacationCommandHandler(RequestHandlerBaseParameters<Vacation> parameters) : base(parameters)
+        {
+        }
+
+        public override async Task<RequestResult<AddVacationDto>> Handle(AddVacationCommand request, CancellationToken cancellationToken)
+        {
+            var isExist =await _mediator.Send(new CheckVacationExistwithName(request.Name));
+            if (isExist)
+                return RequestResult<AddVacationDto>.Failure("A vacation with this name already exists.", ErrorCode.DuplicateRecord);
+
+            var vacation = request.Adapt<Vacation>();
+            await _repository.AddAsync(vacation, cancellationToken);
+            var addVacationDto = vacation.Adapt<AddVacationDto>();
+
+            return RequestResult<AddVacationDto>.Success(addVacationDto);
+        }
+    }
+
+
+
+}
---
+++ HrManagementSystem/Features/ConfigurationsManagement/VacationMangement/AddVacation/DTOs/AddVacationDto.cs
@@ -0,0 +1,18 @@
+﻿using HrManagementSystem.Common.Enums.FeatureEnums;
+using System.ComponentModel.DataAnnotations;
+
+namespace HrManagementSystem.Features.ConfigurationsManagement.VacationMangement.AddVacation.DTOs
+{
+    public class AddVacationDto
+    {
+        public string Name { get; set; } = null!;
+
+    
+        public VacationType Type { get; set; }
+
+        public int DurationInDays { get; set; }
+
+       
+        public string? Description { get; set; }
+    }
+}
