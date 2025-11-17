c23bd0d04ad63eba189ce2fe93745311103e90e5
mahmoud abdelaziz <mahmoudabdelazi962@gmail.com>
2025-11-17 16:56:27 +0200

    AddProbationCommand

---
+++ HrManagementSystem/Features/ConfigurationsManagement/ProbationMangement/AddProbation/DTOs/AddProbationDto.cs
@@ -0,0 +1,16 @@
+﻿using HrManagementSystem.Common.Enums.FeatureEnums;
+using System.ComponentModel.DataAnnotations;
+
+namespace HrManagementSystem.Features.ConfigurationsManagement.ProbationMangement.AddProbation.DTOs
+{
+    public class AddProbationDto
+    {
+        public int DurationInDays { get; set; }
+
+       
+        public string? EvaluationCriteria { get; set; }
+
+        
+        public ProbationStatus Status { get; set; } = ProbationStatus.Active;
+    }
+}
