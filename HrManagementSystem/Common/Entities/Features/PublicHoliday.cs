b20894a2f0c5a09c1b231af04c2615382064da69
mahmoud abdelaziz <mahmoudabdelazi962@gmail.com>
2025-11-25 20:22:22 +0200

    Add-PublicHolidayTable&TypeHolidayEnum

---
+++ HrManagementSystem/Common/Entities/Features/PublicHoliday.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HrManagementSystem.Common.Entities.Features
+{
+    public class PublicHoliday : BaseModel
+    {
+        [Required, MaxLength(200)]
+        public string Name { get; set; } = null!;
+
+        
+        [Column(TypeName = "date")]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public PublicHolidayType Type { get; set; } = PublicHolidayType.National;
+
+        [Required, MaxLength(100)]
+        public string Country { get; set; } = "EG";
+
+        [MaxLength(100)]
+        public string? ExternalSource { get; set; }  
+
+        [MaxLength(200)]
+        public string? ExternalKey { get; set; }    
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
+    }
+}
---
+++ HrManagementSystem/Common/Enums/FeatureEnums/PublicHolidayType.cs
@@ -0,0 +1,12 @@
+﻿namespace HrManagementSystem.Common.Enums.FeatureEnums
+{
+    public enum PublicHolidayType
+    {
+        Unknown = 0,
+        National = 1,
+        Religious = 2,
+        Observance = 3,
+        Optional = 4
+
+    }
+}
